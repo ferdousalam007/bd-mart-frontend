e3e0ac5be579fc63a68962fecb93b0751762f6d9
ferdousalam007 <iamferdousalam007@gmail.com>
2024-12-17 20:48:58 +0600

    product  recent views done

--- src/pages/RecentViews.tsx
+++ src/pages/RecentViews.tsx
@@ -4,31 +4,31 @@ import { useLocalStorageState } from "../utils/useLocalStorageState";
 
 const RecentViews = () => {
   const [recentViews] = useLocalStorageState([], "recentViews");
-
+console.log(recentViews,"recentViews");
   return (
     <section className="container mx-auto  lg:py-14 py-10 px-5">
       <SectionTitle
         title="Recent Views"
         description={"Your recently viewed products are displayed here."}
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {recentViews.map((product: any) => (
           <Link
             to={`/products/${product._id}`}
             key={product._id}
-            className="bg-primary-background p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+            className="border rounded-lg shadow-sm p-4  hover:shadow-lg transition duration-300 bg-white"
           >
             <img
               src={product.image}
               alt={product.name}
-              className="w-full h-52 object-contain rounded-lg mb-4 bg-[rgba(109,123,255,0.2)]"
+              className="object-contain max-h-full max-w-full"
             />
-            <h3 className="text-lg font-bold text-primary-text mb-2">
+            <h3 className="text-primary text-lg font-medium mt-3 mb-2 hover:text-slate-700 cursor-pointer">
               {product.name}
             </h3>
-            <p className="text-success-color font-semibold mb-4  text-lg">
-              ${product.price}
-            </p>
+            <span className="text-red-500 text-lg font-bold">
+              ${product.price.toFixed(2)}
+            </span>
           </Link>
         ))}
       </div>
