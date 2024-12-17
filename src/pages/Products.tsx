6a14c694482ce750536687c03db8a210bfd954a0
ferdousalam007 <iamferdousalam007@gmail.com>
2024-12-17 19:50:28 +0600

    product views work

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -44,37 +44,94 @@ const Products = () => {
             title="All Products"
             description={`Browse all products in this shop`}
           />
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {/* Product Grid */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-4">
             {products.map((product: any) => (
               <div
                 key={product._id}
-                className="relative bg-primary-background p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow ease-in duration-300"
+                className="border rounded-lg shadow-sm p-4  hover:shadow-lg transition duration-300 bg-white"
               >
-                <Link to={`/products/${product._id}`}>
-                  <img
-                    src={product.images[0]}
-                    alt={product.name}
-                    className="w-full h-52 object-contain rounded-lg mb-4 bg-[rgba(109,123,255,0.2)]"
-                  />
-                  {product.discount > 0 && (
-                    <div className="absolute top-4 right-4 bg-error-color   text-white text-xs font-bold py-1 px-3 rounded-full shadow-lg">
-                      {product.discount}% OFF
-                    </div>
-                  )}
-                  <h3 className="text-lg font-bold text-primary-text mb-2">
+                {/* Discount Badge */}
+                {product.discount && (
+                  <span className="bg-red-500 text-white text-xs font-bold px-2 py-1 rounded absolute">
+                    {product.discount}% OFF
+                  </span>
+                )}
+                {/* Product Image */}
+                <Link to={`/products/${product._id}`} className="block mb-4">
+                  <div className="w-full h-36 flex items-center justify-center mb-4">
+                    <img
+                      src={product.images[0]}
+                      alt={product.name}
+                      className="object-contain max-h-full max-w-full"
+                    />
+                  </div>
+
+                  <h3 className="text-primary text-lg font-semibold mb-2 hover:text-slate-700 cursor-pointer">
                     {product.name}
                   </h3>
-                  <p className="text-success-color font-semibold mb-4  text-lg">
-                    ${product.price}
-                  </p>
                 </Link>
-
-                <div className="flex justify-between gap-3">
+                <div className="flex  flex-col mb-2">
+                  <span className="text-red-800 text-sm mr-1 uppercase underline mb-2">
+                    <Link to={`/products?category=${product.category._id}`}>
+                      <span className="font-bold text-slate-800">
+                        {" "}
+                        Category :{" "}
+                      </span>{" "}
+                      {product.category.name}
+                    </Link>
+                  </span>
+                  <Link to={`/shops/${product.shop._id}`}>
+                    <span className="text-red-800 text-sm mr-1 uppercase pt-2 underline">
+                      <span className="font-bold text-slate-800"> shop : </span>
+                      {product.shop.name}
+                    </span>
+                  </Link>
+                </div>
+                {/* Prices */}
+                <div className="flex items-center gap-2 mb-4">
+                  <span className="text-red-500 text-lg font-bold">
+                    $
+                    {(
+                      product.price -
+                      (product.price * product.discount) / 100
+                    ).toFixed(2)}
+                  </span>
+                  <span className="text-gray-400 line-through">
+                    ${product.price.toFixed(2)}
+                  </span>
+                </div>
+                {/* CTA Buttons */}
+                <div className="flex flex-col gap-2">
                   <AddToCartButton product={product} />
-
                   <CompareButton product={product} />
                 </div>
               </div>
+              // <div
+              //   key={product._id}
+              //   className="relative bg-primary-background p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow ease-in duration-300"
+              // >
+              //   <img
+              //     src={product.images[0]}
+              //     alt={product.name}
+              //     className="w-full h-52 object-contain rounded-lg mb-4 bg-[rgba(109,123,255,0.2)]"
+              //   />
+              //   {product.discount > 0 && (
+              //     <div className="absolute top-4 right-4 bg-error-color   text-white text-xs font-bold py-1 px-3 rounded-full shadow-lg">
+              //       {product.discount}% OFF
+              //     </div>
+              //   )}
+              //   <h3 className="text-lg font-bold text-primary-text mb-2">
+              //     {product.name}
+              //   </h3>
+              //   <p className="text-success-color font-semibold mb-4  text-lg">
+              //     ${product.price}
+              //   </p>
+              //   <div className="flex justify-between gap-3">
+              //     <AddToCartButton product={product} />
+              //     <CompareButton product={product} />
+              //   </div>
+              // </div>
             ))}
           </div>
         </section>
@@ -84,7 +141,7 @@ const Products = () => {
         pageCount={Math.ceil(totalProducts / productsPerPage)}
         onPageChange={handlePageChange}
         containerClassName="flex justify-center items-center space-x-2 mt-5"
-        activeClassName="bg-primary-brand text-primary-white"
+        activeClassName="bg-primary-brand text-slate-800"
         pageClassName="px-3 py-1 rounded-md border border-primary-grey"
         previousLabel="<"
         nextLabel=">"
